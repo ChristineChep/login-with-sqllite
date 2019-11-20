a167d5ac244b552aed401512b29f6ce8391720a2
ChristineChep <39522563+ChristineChep@users.noreply.github.com>
2019-11-20 13:08:01 +0300

    Updated Input Validation file

--- helper/InputValidation.java
+++ helper/InputValidation.java
@@ -1,14 +1,61 @@
 package myapp.user.example.com.sikika.helper;
 
+import android.app.Activity;
 import android.content.Context;
+import android.text.Layout;
+import android.util.Patterns;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
-public class InputValidation {
+public class InputValidation { 
     private Context context;
+    private Object EditText;
 
-    public InputValidation (Context context){
+    public InputValidation(Context context) {
         this.context = context;
     }
 
-    public boolean isInputEditTextFilled(EditText textInputEditText.TextInputLayout textInputLayout.String message)
+        public boolean isInputEditTextFilled(EditText  editText, Layout layout, String message){
+            String value = editText.getText().toString().trim();
+            if (value.isEmpty()) {
+                layout.setError(message);
+                hideKeyboardFrom(editText);
+                return false;
+            } else {
+                layout.setErrorEnabled(false);
+            }
+            return true;
+        }
+        public boolean isInputEditTextEmail (EditText editText, Layout layout,String message){
+            String value = editText.getText().toString().trim();
+            if(value.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(value).matches()){
+                layout.setError(message);
+                hideKeyboardFrom(editText);
+                return false; 
+            }else {
+                layout.setErorEnabled(false);
+            }
+            return true;
+    }
+
+        public boolean isInputEditTextmatches(EditText editText1, EditText editText2, Layout layout, String message ){
+        String value1 = editText1.getText().toString().trim();
+        String value2 = editText2.getText().toString().trim();
+        if(!value1.contentEquals(value2)){
+            layout.setError(message);
+            hideKeyboardFrom(editText2);
+            return false;
+        }else{
+            layout.setErrorEnabled(false); 
+        }
+        return true;
+        }
+
+        private void hideKeyboardFrom(View view){
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        }
 }
+
